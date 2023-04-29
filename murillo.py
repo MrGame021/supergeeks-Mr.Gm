7680f23e9f4084ff1f93ef8bb2a664dca6790dca
MrGame021 <130846611+MrGame021@users.noreply.github.com>
2023-04-29 11:59:39 -0300

    Add files via upload
    aula dia 29/04/23

---
+++ murillo.py
@@ -0,0 +1,24 @@
+print("Ex1:\n")
+nome1 = input("Qual o seu nome?\n")
+print("Olá",nome1,"! Seja muito bem vindo!!")
+
+print("-------------------------------------------------------------------------------\nEx2:\n")
+
+a = input("Digite o primeiro nome:")
+b = input("Digite o segundo nome:")
+c = input("Digite o terceiro nome:")
+d = input("Por fim, digite o quarto nome:")
+print(b,";\n",c,";\n",a,";\n",d,".")
+
+print("-------------------------------------------------------------------------------\nEx3:\n")
+
+e = input("Em um lugar com eco, você gosta de gritar com A ou com O?\n")
+print("Então, seu grito iria ser parecido como:\n", e*18)
+print("-------------------------------------------------------------------------------\nEx4:\n")
+
+f = input("Escreva um nome: ")
+g = input("Escreva outro nome: ")
+print(f,"S2",g)
+
+print("-------------------------------------------------------------------------------\nEx5:\n")
+
