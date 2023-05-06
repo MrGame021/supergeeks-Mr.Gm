a487d7104cc03dc85d287a93b2371f9be67b00b0
MrGame021 <130846611+MrGame021@users.noreply.github.com>
2023-05-06 11:45:02 -0300

    Add files via upload

--- murillo.py
+++ murillo.py
@@ -3,7 +3,6 @@ nome1 = input("Qual o seu nome?\n")
 print("Olá",nome1,"! Seja muito bem vindo!!")
 
 print("-------------------------------------------------------------------------------\nEx2:\n")
-
 a = input("Digite o primeiro nome:")
 b = input("Digite o segundo nome:")
 c = input("Digite o terceiro nome:")
@@ -11,14 +10,46 @@ d = input("Por fim, digite o quarto nome:")
 print(b,";\n",c,";\n",a,";\n",d,".")
 
 print("-------------------------------------------------------------------------------\nEx3:\n")
-
 e = input("Em um lugar com eco, você gosta de gritar com A ou com O?\n")
 print("Então, seu grito iria ser parecido como:\n", e*18)
-print("-------------------------------------------------------------------------------\nEx4:\n")
 
+print("-------------------------------------------------------------------------------\nEx4:\n")
 f = input("Escreva um nome: ")
 g = input("Escreva outro nome: ")
 print(f,"S2",g)
 
 print("-------------------------------------------------------------------------------\nEx5:\n")
+h = int(input("Digite um número: "))
+i = int(input("Digite outro número: "))
+print("A soma desses dois números é igual a:",h+i)
+
+print("-------------------------------------------------------------------------------\nEx6:\n")
+j = int(input("Escreva um número: "))
+k = int(input("Escreva outro número: "))
+print("A soma de",j,"e",k,"é: ",j+k)
+print("A subtração de",j,"e",k,"é: ",j-k)
+print("A divisão de",j,"e",k,"é: ",j/k)
+print("A multiplicação de",j,"e",k,"é: ",j*k)
+
+print("-------------------------------------------------------------------------------\nEx7:\n")
+l = int(input("Informe quantos litros você tem em seu tanque/galão de combustível?\n"))
+m = int(input("Informe quantos litros você gasta de combustível por Km percorrido?\n"))
+print("Sabendo disso, você consegue percorrer",l/m,"Km de distância")
+
+print("-------------------------------------------------------------------------------\nEx8:\n")
+n = int(input("Digite um número para servir de base:  "))
+o = 2*3.14*n
+print("Como base o número",n,", o seu círculo tem",o,"de circunferencia")
+
+print("-------------------------------------------------------------------------------\nEx9:\n")
+print("Cº ~ Kº:\n")
+p = int(input("Escreva uma temperatura em celscius: "))
+q = p+273.15
+print("Essa temperatura, convertida para kelvin, é:",q)
+
+
+print("Cº ~ Fº:\n")
+r = int(input("Escreva uma outra temperatura em celcius: "))
+s = (r/5)*9+32
+print("Essa temperatura, convertida para fahrenheit, é:",s)
 
