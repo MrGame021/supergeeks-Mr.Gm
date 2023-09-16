d986e6c2ab684761b7196937c8ec844165e2077a
MrGame021 <130846611+MrGame021@users.noreply.github.com>
2023-09-16 12:06:12 -0300

    Update a.py

--- a.py
+++ a.py
@@ -1,21 +1,69 @@
-import random
-import os
+import random, os
+
 # os.system('cls')
-a = "=-"*10+"="
 
+a = "=-"*8+"="
+print(f'''
+ -----------------------------------------------
+|                                               |
+|                                               |
+|{a}    Forca    {a}|
+|                                               |
+|                                               |
+|                                               |
+|                                               |
+|                                               |
+|                                               |
+|                                               |
+|                                               |
+|                                               |
+|                                               |          
+|                                               |
+ -----------------------------------------------
+''')
 
-p4 = ['Urso', 'Caos', 'Vida', 'Auto', 'Doce']
+p4 = ['Urso', 'Caos', 'Copo', 'Alto', 'Doce']
+d4 = ['Animal', 'Situação', 'Cosinha', 'Altura', 'Gosto/Sentimento']
+
 p7 = ['Celular', 'Abóbora', 'Etienne', 'Empatia', 'Família']
-p8 = ['Capivara', 'Suricato', 'Panelada', 'Nintendo', 'Educação']
-p10 = ['Computador', 'Comparador', 'Dicionário', 'Maturidade', 'Referência']
+d7 = ['Eletrônico', 'Comida', 'Nome', 'Sentimento', 'Pais']
+
+p8 = ['Capivara', 'Suricato', '', 'Nintendo', 'Educação']
+d8 = ['Animal', 'Animal','','Desenvolvedora de Jogos','Estudo']
+
+p10 = ['Computador', 'Fleumático', 'Dicionário', 'Maturidade', 'Pragmático']
+d10 = ['Eletrônico','Muito Paciente','Palavras','Crescimento','Ordem Prática']
+
 p11 = ['Playstation', 'Perspectiva', 'Dissimulado', 'Compreensão', 'Preconceito']
+d11 = ['Desenvolvedora de Jogos','Ponto de Vista','','Entendimento','']
 
 
-while True:
-    print(f"{a}  Acerte a palavra  {a}")
-    b = int(input("""
+c = input('''
  ----------------------------------------------
-|    Digite um número de 1 a 5:                |
+|        Escolha a dificuldade do jogo:        |
+|                                              |
+|                                              |
+|    A - Normal                                |
+|                                              |
+|                                              |
+|    B - Difícil                               |
+|                                              |
+|                                              |
+|    C - Muito Difícil                         |
+|                                              |
+|                                              |
+|                                              |          
+|                                              |
+ ----------------------------------------------
+
+''')
+
+if(c == A or c == a):
+    while True:
+        print(f"{a}  Acerte a palavra  {a}")
+        b = int(input('''
+ ----------------------------------------------
+|          Digite um número de 1 a 5:          |
 |                                              |
 |                                              |
 |    1) _ _ _ _                                |
@@ -30,132 +78,39 @@ while True:
 |                                              |          
 |                                              |
  ----------------------------------------------
-"""))
-    text = ''
+'''))
+        text = ''
 
-    if(b == 1):
-      p = p4[random.randint(0, len(p4)-1)]
-      for i in range(len(p)):
-        text += '_ '
-      print(text)
-      if(p == 'Urso'):
-          print("\nDica: Animal")
-          input()
-      elif(p == 'Caos'):
-          print("\nDica: Situação")
-          input()
-      elif(p == 'Vida'):
-          print("\nDica: S2")
-          input()
-      elif(p == 'Auto'):
-          print("\nDica: Altura")
-          input()
-      elif(p == 'Doce'):
-          print("\nDica: Gosto/Comida")
-          input()
-      
-      
-
-      
-    elif(b == 2):
-      p = p7[random.randint(0, len(p7)-1)]
-      for i in range(len(p)):
-        text += '_ '
-      print(text)
-      if(p == 'Celular'):
-          print("\nDica: Eletrônico")
-          input()
-      elif(p == 'Abóbora'):
-          print("\nDica: Comida")
-          input()
-      elif(p == 'Etienne'):
-          print("\nDica: Nome")
-          input()
-      elif(p == 'Empatia'):
-          print("\nDica: Sentimento")
-          input()
-      elif(p == 'Família'):
-          print("\nDica: Pais")
-          input()
-      
-
-      
-    elif(b == 3):
-      p = p8[random.randint(0, len(p8)-1)]
-      for i in range(len(p)):
-        text += '_ '
-      print(text)
-      if(p == 'Capivara'):
-          print("\nDica: Animal")
-          input()
-      elif(p == 'Suricato'):
-          print("\nDica: Animal")
-          input()
-      elif(p == 'Panelada'):
-          print("\nDica: Tipo de Protesto")
-          input()
-      elif(p == 'Nintendo'):
-          print("\nDica: Desenvolvedora de Jogos")
-          input()
-      elif(p == 'Educação'):
-          print("\nDica: Ensino")
-          input()
-      
-
-
-      
-    elif(b == 4):
-      p = p10[random.randint(0, len(p10)-1)]
-      for i in range(len(p)):
-        text += '_ '
-      print(text)
-      if(p == 'Computador'):
-          print("\nDica: Eletrônico")
-          input()
-      elif(p == 'Comparador'):
-          print("\nDica: Compara")
-          input()
-      elif(p == 'Dicionário'):
-          print("\nDica: Palavras")
-          input()
-      elif(p == 'Maturidade'):
-          print("\nDica: Crescimento")
-          input()
-      elif(p == 'Referência'):
-          print("\nDica: ??")
-          input()
-      
-
-
-      
-    elif(b == 5):
-      p = p11[random.randint(0, len(p11)-1)]
-      for i in range(len(p)):
-        text += '_ '
-      print(text)
-      if(p == 'Playstation'):
-          print("\nDica: Desenvolvedora de Jogos")
-          input()
-      elif(p == 'Perspectiva'):
-          print("\nDica: Ponto de Vista")
-          input()
-      elif(p == 'Dissimulado'):
-          print("\nDica: ??")
-          input()
-      elif(p == 'Compreensão'):
-          print("\nDica: Entendimento")
-          input()
-      elif(p == 'Preconceito'):
-          print("\nDica: ??")
-          input()
-      
-
-      
-    else:
-        print("Essa opção não existe ou ainda não foi incrementada!")
-        input()
-        break
-    os.system('cls')
-
-print('oi')
+        
+        if(b == 1):
+            x = random.randint(0, len(p4)-1)
+            p = p4[x]
+            d = d4[x]
+              
+        elif(b == 2):
+            x = random.randint(0, len(p7)-1)
+            p = p7[x]
+            d = d7[x]
+          
+        
+        elif(b == 3):
+            x = random.randint(0, len(p8)-1)
+            p = p8[x]
+            d = d8[x]
+          
+        elif(b == 4):
+            x = random.randint(0, len(p10)-1)
+            p = p10[x]
+            d = d10[x]
+          
+        elif(b == 5):
+            x = random.randint(0, len(p11)-1)
+            p = p11[x]
+            d = d11[x]
+        else:
+            print("Essa opção não existe ou ainda não foi incrementada!")
+            input()
+            break
+        os.system('cls')
+print("oi")
 input()
